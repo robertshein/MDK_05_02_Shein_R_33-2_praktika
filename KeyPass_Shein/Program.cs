4a40a5f3cc22500bbaff8d0216f111668c0d2566
robertshein <robertshein21@gmail.com>
2026-06-08 11:20:24 +0500

    Подключение Swagger для сервера. Шейн

--- KeyPass_Shein/Program.cs
+++ KeyPass_Shein/Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddMvc(option => option.EnableEndpointRouting = true);
+builder.Services.AddSwaggerGen(option => {
+    option.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Инструментарий"
+    });
+    string PathFile = Path.Combine(AppContext.BaseDirectory, "KeyPass_Shein.xml");
+    option.IncludeXmlComments(PathFile);
+});
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Инструментарий");
+});
+app.UseRouting();
+app.UseEndpoints(endpoints => 
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
